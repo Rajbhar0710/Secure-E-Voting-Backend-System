1e29da6eee1506d92d0b16ac91f2e8f9f7ac7664
Raj <rajbahr321@gmail.com>
2026-03-10 23:12:04 +0530

    upadate backend

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const db = require('./db');
+
+// Load environment variables from backend/.env
+require('dotenv').config({ path: path.join(__dirname, '.env') });
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json()); // req body
+const PORT = process.env.PORT || 3000;
+
+// import the router files
+const userRoutes = require('./routes/userRoutes');
+const candidateRoutes = require('./routes/candidateRoutes');
+
+// use the routes
+app.use('/user', userRoutes);
+app.use('/candidates', candidateRoutes);
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
